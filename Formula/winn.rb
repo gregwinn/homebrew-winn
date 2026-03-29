0eebf32c7f22e6474dc628fe3268fcc06750d0f4
Greg Winn <winn.greg@gmail.com>
2026-03-28 22:28:29 -0500

    Update winn to v0.3.1

--- Formula/winn.rb
+++ Formula/winn.rb
@@ -1,8 +1,8 @@
 class Winn < Formula
   desc "A Ruby/Elixir-inspired language that compiles to the BEAM"
   homepage "https://github.com/gregwinn/winn-lang"
-  url "https://github.com/gregwinn/winn-lang/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "addf7e609ceb9ca51287a624134b1245fe8d43c4603bad5e5d0dc056d3e3cc25"
+  url "https://github.com/gregwinn/winn-lang/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "6ba548bf0abdcb688f8dcb51aaf0bc9eb4894c4bc000458b973c9646b1850bb5"
   license "MIT"
 
   depends_on "erlang"
@@ -14,10 +14,8 @@ class Winn < Formula
   end
 
   test do
-    # Verify the CLI runs
     assert_match "Winn - a compiled language", shell_output("#{bin}/winn help")
 
-    # Verify compilation works
     (testpath/"hello.winn").write <<~EOS
       module Hello
         def main()
