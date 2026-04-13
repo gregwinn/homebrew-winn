0ef0e559a580b8dcd96a923bbd66728f6529daae
Greg Winn <winn.greg@gmail.com>
2026-04-12 20:38:55 -0500

    Update formula to v0.9.1

--- Formula/winn.rb
+++ Formula/winn.rb
@@ -1,8 +1,8 @@
 class Winn < Formula
   desc "A Ruby/Elixir-inspired language that compiles to the BEAM"
   homepage "https://github.com/gregwinn/winn-lang"
-  url "https://github.com/gregwinn/winn-lang/archive/refs/tags/v0.9.0.tar.gz"
-  sha256 "3509315ce3e3e1a835736bb355efaa90371f53b48bcc8e2730f80d990a918398"
+  url "https://github.com/gregwinn/winn-lang/archive/refs/tags/v0.9.1.tar.gz"
+  sha256 "a7c99f0501947b1c4d45b8176db28a465bb47f8a2e795fc4465bced4d7a48162"
   license "MIT"
   depends_on "erlang"
   depends_on "rebar3"
